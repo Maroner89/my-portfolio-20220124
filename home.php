d81c727b965effe370dabb1361ed2e5a300cac7e
maroner89 <mjmotors7@gmail.com>
2022-01-20 21:14:29 +0900

    first commit

---
+++ home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>조용운 포트폴리오</title>
+
+    <!-- 스와이프 CSS 링크 https://swiperjs.com/get-started -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
+    <!-- 폰트어썸 CDN 링크 https://cdnjs.com/libraries?q=fo -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- 커스텀 CSS 파일 링크 -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <!-- 헤더 섹션 시작 -->
+
+    <section class="header">
+
+        <a href="home.php" class="logo">Maroner.</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+
+    </section>
+    <!-- 헤더 섹션 끝 -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- 스와이퍼 자바 스크립트 링크 https://swiperjs.com/get-started -->
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+    <!-- 커스텀 자바스크립트 파일 링크 -->
+    <script src="js/script.js"></script>
+</body>
+</html>
