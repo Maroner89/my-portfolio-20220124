8bcf60fd73d68c2b5b3579456a96557c1c77e0ea
maroner89 <mjmotors7@gmail.com>
2022-01-25 13:25:06 +0900

    third commit

--- home.php
+++ home.php
@@ -35,11 +35,111 @@
     </section>
     <!-- 헤더 섹션 끝 -->
 
+    <!-- 서비스 섹션 시작 -->
+
+    <section class="services">
+
+        <h1 class="heading-title"> 나의 포트폴리오2</h1>
+
+        <div class="box-container">
+
+            <div class="box">
+                <img src="img/icon-1.png" alt="icon1">
+                <h3>adventure</h3>
+            </div>
+
+            <div class="box">
+                <img src="img/icon-2.png" alt="icon2">
+                <h3>tour guide</h3>
+            </div>
+
+            <div class="box">
+                <img src="img/icon-3.png" alt="icon3">
+                <h3>trekking</h3>
+            </div>
+
+            <div class="box">
+                <img src="img/icon-4.png" alt="icon4">
+                <h3>camp fire</h3>
+            </div>
+
+            <div class="box">
+                <img src="img/icon-5.png" alt="icon5">
+                <h3>off road</h3>
+            </div>
+
+            <div class="box">
+                <img src="img/icon-6.png" alt="icon6">
+                <h3>camp fire</h3>
+            </div>
+
+        </div>
+
+    </section>
 
 
 
 
 
+    <!-- 서비스 섹션 끝 -->
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- footer 섹션 시작 -->
+
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i> package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i> book</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i> +82 10-123-4567</a>
+                <a href="#"><i class="fas fa-phone"></i> +82 10-123-4567</a>
+                <a href="#"><i class="fas fa-envelope"></i> mj_motors@naver.com</a>
+                <a href="#"><i class="fas fa-map"></i> Su-won, Korea</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+                <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+                <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+                <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+            </div>
+        </div>
+
+
+        <div class="credit">
+            created by <span>Maroner</span> | all rights reserved.
+        </div>
+    </section>
+
+    <!-- footer 섹션 끝 -->
 
 
 
---
+++ package.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>소개</title>
+
+    <!-- 스와이프 CSS 링크 https://swiperjs.com/get-started -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
+    <!-- 폰트어썸 CDN 링크 https://cdnjs.com/libraries?q=fo -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- 커스텀 CSS 파일 링크 -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <!-- 헤더 섹션 시작 -->
+
+    <section class="header">
+
+        <a href="home.php" class="logo">Maroner.</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+    <!-- 헤더 섹션 끝 -->
+
+    <div class="heading" style="background:url(img/header-bg-2.png) no-repeat">
+        <h1>포트폴리오</h1>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- footer 섹션 시작 -->
+
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i> package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i> book</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i> +82 10-123-4567</a>
+                <a href="#"><i class="fas fa-phone"></i> +82 10-123-4567</a>
+                <a href="#"><i class="fas fa-envelope"></i> mj_motors@naver.com</a>
+                <a href="#"><i class="fas fa-map"></i> Su-won, Korea</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+                <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+                <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+                <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+            </div>
+        </div>
+
+
+        <div class="credit">
+            created by <span>Maroner</span> | all rights reserved.
+        </div>
+    </section>
+
+    <!-- footer 섹션 끝 -->
+
+    <!-- 스와이퍼 자바 스크립트 링크 https://swiperjs.com/get-started -->
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+    <!-- 커스텀 자바스크립트 파일 링크 -->
+    <script src="js/script.js"></script>
+</body>
+</html>
